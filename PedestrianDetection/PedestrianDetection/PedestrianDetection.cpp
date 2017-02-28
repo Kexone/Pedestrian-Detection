259b02d5e3fe2768d5e6948fc19e8a3b09062801
Kexone <kami.extreme@gmail.com>
2017-02-28 18:39:44 +0100

    Implement MOG & HOG algorithm

---
+++ PedestrianDetection/PedestrianDetection/PedestrianDetection.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+void runMOG() {
+	MOGDetection *mogDetect = new MOGDetection();
+	mogDetect->detect();
+}
+
+void runHOG() {
+	HOGDetection *hogDetect = new HOGDetection();
+	hogDetect->detect();
+}
+
+int main(int argc, char* argv[])
+{
+	//std::thread first(runMOG);
+	std::thread second(runHOG);
+	//first.join();
+	second.join();
+	return 0;
+}
